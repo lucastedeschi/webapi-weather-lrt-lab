c124bdead9f3807c7b3bbefef4124fc45abd9985
Lucas Resende Tedeschi <lucas.tedeschi@onlineapp.com.br>
2021-01-24 19:16:19 -0300

    Removing test from gitIgnore

---
+++ test/WeatlherLrt.Application.Test/Persistence/WeatherLrtTestContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using WeatherLrt.Application.Interfaces;
+using WeatherLrt.Persistence;
+
+namespace WeatlherLrt.Application.Test.Persistence
+{
+    internal sealed class WeatherLrtTestContextFactory
+    {
+        public static IWeatherLrtContext CreateContextForSQLite()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var builder = new DbContextOptionsBuilder<WeatherLrtContext>()
+                .UseSqlite(connection)
+                .ConfigureWarnings(x => x.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.AmbientTransactionWarning));
+
+            var context = new WeatherLrtContext(builder.Options);
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+    }
+}
