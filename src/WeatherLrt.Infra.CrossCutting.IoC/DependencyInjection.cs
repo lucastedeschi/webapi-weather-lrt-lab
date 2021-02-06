1f1926478359d67acf7ab123a7c8aef3659c595b
Lucas Resende Tedeschi <lucas.tedeschi@onlineapp.com.br>
2021-02-05 23:25:13 -0300

    Registering dependency injection

--- src/WeatherLrt.Infra.CrossCutting.IoC/DependencyInjection.cs
+++ src/WeatherLrt.Infra.CrossCutting.IoC/DependencyInjection.cs
@@ -1,16 +1,23 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Net.Http;
+using AutoMapper;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
 using WeatherLrt.Application.Commands.SystemUsers.Create;
 using WeatherLrt.Application.Interfaces;
 using WeatherLrt.Application.Profiles;
 using WeatherLrt.Persistence;
+using WeatherLrt.Services.OpenWeather;
 
 namespace WeatherLrt.Infra.CrossCutting.IoC
 {
     public static class DependencyInjection
     {
+        private const string OpenWeatherCurrentClientName = "OpenWeatherCurrentClientName";
+
         public static void AddDependencies(this IServiceCollection services)
         {
             services.AddMediatR(typeof(CreateSystemUserCommand).Assembly);
@@ -19,7 +26,14 @@ namespace WeatherLrt.Infra.CrossCutting.IoC
 
             services.AddDbContext<IWeatherLrtContext, WeatherLrtContext>(c => c.UseSqlServer("connectionString"), ServiceLifetime.Scoped);
 
-            //services.AddScoped<IOpenWeatherServiceClient, OpenWeatherServiceClient>();
+            services.AddHttpClient(OpenWeatherCurrentClientName)
+                .AddPolicyHandler(GetRetryPolicy());
+
+            services.AddScoped<IOpenWeatherServiceClient, OpenWeatherServiceClient>();
         }
+
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy() =>
+            HttpPolicyExtensions.HandleTransientHttpError()
+                .WaitAndRetryAsync(2, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
     }
 }
--- src/WeatherLrt.Services/OpenWeather/OpenWeatherServiceClient.cs
+++ src/WeatherLrt.Services/OpenWeather/OpenWeatherServiceClient.cs
@@ -10,6 +10,7 @@ namespace WeatherLrt.Services.OpenWeather
 {
     public sealed class OpenWeatherServiceClient : IOpenWeatherServiceClient
     {
+        private const string OpenWeatherCurrentClientName = "OpenWeatherCurrentClientName";
         private const string Endpoint = "http://api.openweathermap.org/data/2.5";
         private const string AppId = "0cc25201bf9bd27c53917b8ead2508e0";
         private const string InternalErrorMessage = "An error has occurred while communicating with OpenWeatherMap. Please contact the support team";
@@ -18,9 +19,9 @@ namespace WeatherLrt.Services.OpenWeather
         private readonly HttpClient _httpClient;
         private readonly ILogger<OpenWeatherServiceClient> _logger;
 
-        public OpenWeatherServiceClient(string clientName, IHttpClientFactory clientFactory, ILogger<OpenWeatherServiceClient> logger)
+        public OpenWeatherServiceClient(IHttpClientFactory clientFactory, ILogger<OpenWeatherServiceClient> logger)
         {
-            _httpClient = clientFactory.CreateClient(clientName);
+            _httpClient = clientFactory.CreateClient(OpenWeatherCurrentClientName);
             _logger = logger;
         }
 
