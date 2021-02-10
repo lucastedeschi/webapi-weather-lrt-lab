faac8403502150fad07a373acd67ed1225a473b5
Lucas Resende Tedeschi <lucas.tedeschi@onlineapp.com.br>
2021-02-10 20:25:51 -0300

    Adding authorization in the Swagger

--- src/WeatherLrt.WebApi/Startup.cs
+++ src/WeatherLrt.WebApi/Startup.cs
@@ -1,9 +1,9 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using WeatherLrt.Infra.CrossCutting.IoC;
 
 namespace WeatherLrt.WebApi
@@ -21,7 +21,40 @@ namespace WeatherLrt.WebApi
         {
             services.AddControllers();
 
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "WeatherLrt Web API"
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter 'Bearer' [space] and then your valid token in the text input below (Example: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\")",
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                          },
+                          new string[] {}
+
+                    }
+                });
+            });
 
             services.AddRouting(options => options.LowercaseUrls = true);
 
